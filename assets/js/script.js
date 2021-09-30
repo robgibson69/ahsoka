4fd7bf09bbe933f173d67f490eea13566f7f405e
Makda Girmay <makdagirmay4@gmail.com>
2021-09-30 15:08:18 -0400

    basics of slide out list

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,14 @@ document.addEventListener('click', (e) => {
     }
 
 });
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
