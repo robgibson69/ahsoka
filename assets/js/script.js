9087b06844b4e1be0c1378eeddfe08e722e4a115
Dave Quinn <dquinn8@cogeco.ca>
2021-09-30 20:11:39 -0400

    added fetch recipe and started diplay recipe

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ const searchByIngredient = (searchString) => {
             if (response.ok) {
                 response.json()
                     .then((data) => {
-                        console.log(data);
+                        //console.log(data);
                         displayMeals(data.meals);
                     })
             } else {
@@ -27,6 +27,62 @@ const searchByIngredient = (searchString) => {
         .catch(err => {
             console.error(err);
         });
+
+}
+
+const fetchRecipe = (idNum) => {
+
+    if (idNum !== '') {
+
+        fetch("https://themealdb.p.rapidapi.com/lookup.php?i=" + idNum, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "themealdb.p.rapidapi.com",
+                    "x-rapidapi-key": "c5d39432acmsh9d55200b1fddc5ap16e8f6jsn9b22759a0fe2"
+                }
+            })
+            .then(response => {
+                if (response.ok) {
+                    response.json()
+                        .then((data) => {
+                            console.log(data);
+                            displayRecipe(data.meals[0]);
+                        })
+                } else {
+                    console.error(err);
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
+
+    }
+
+}
+
+const displayRecipe = (meal) => {
+
+    let name = meal.strMeal;
+    let image = meal.strMealThumb;
+    let source = meal.strSource;
+    let video = meal.strYoutube;
+
+    let recipe = meal.Instructions;
+
+    let ingredient = [];
+    let measure = [];
+
+    let opt = '';
+    for (let c = 0; c < 2; c++) { //run a loop twice// once for ingredients and once for measure
+        c ? opt = 'Measure' : opt = 'Ingredient' // when c is 1 opt = Measure/when c is 0 opt = ingredient
+        for (let i = 0; i < 20; i++) {
+            if (meal['str' + opt + i] !== "") {
+                c ? measure.push(meal['str' + opt + i]) : ingredient.push(meal['str' + opt + i]);
+            }
+        }
+    }
+
+    alert(ingredient);
 
 }
 
@@ -47,14 +103,25 @@ const displayMeals = (meals) => {
 
             $('#searchOutput').append(card);
         });
+
+        $('.meal-container').on('click', (e) => {
+            let id = $(e.target).closest('div').attr('data-mealID');
+
+            fetchRecipe(id);
+
+        });
     }
 }
 
+
+$('#ingredientSearch').on('keypress', (e) => {
+    if (e.key === 'Enter') {
+        document.querySelector('#city-search-button').click();
+    }
+});
+
 document.addEventListener('click', (e) => {
-    if (e.target.closest('div').matches('.meal-container')) {
-        alert('recipe clicked ' + $(e.target).closest('div').attr('data-mealID'));
-        return;
-    } else if (e.target.id === "searchBtn") {
+    if (e.target.id === "searchBtn") {
         searchByIngredient();
     } else {
         // alert(e.target.id);
