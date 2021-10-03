5fc41833f4525d6be3963c4ce9ea359777c15975
Dave Quinn <dquinn8@cogeco.ca>
2021-10-03 14:59:42 -0400

    added logo

---
+++ assets/js/D-script.js
@@ -0,0 +1,8 @@
+$('#searchOutput').append(
+    $('<div>')
+    .css('width', '100%')
+    .css('margin-top', '5rem')
+    .css('background-image', "url('./assets/images/logo1.png')")
+    .css('background-size', 'contain')
+    .css('background-repeat', 'no-repeat')
+);
