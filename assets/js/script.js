d78c41c3f8da14dfb9b3fefc1014be7e3de22b88
Makda Girmay <makdagirmay4@gmail.com>
2021-10-09 00:59:02 -0400

    adding the drink function work to the main script file and commenting anything that needs to be clarified

--- assets/js/script.js
+++ assets/js/script.js
@@ -301,6 +301,48 @@ const fetchRecipe = (idNum) => {
 
 }
 
+//  ******  URL FOR 1 RANDOM COCKTAIL  ******  //
+var getRandCocktail = function() {
+    getCocktails("https://the-cocktail-db.p.rapidapi.com/random.php");
+
+};
+
+
+//  ****** URL FOR 10 RANDOM COCKTAILS  ******  //
+var get10RandCocktail = function() {
+    getCocktails('https://the-cocktail-db.p.rapidapi.com/randomselection.php');
+};
+
+
+//  ****** GET THE COCKTAIL(S)  ******  //
+var getCocktails = function(fetchURL) {
+    fetch(fetchURL, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+                "x-rapidapi-key": "9fbe0f4555msh10b0fd4cdc4f9abp142f2djsn3d0f0dff927e"
+            }
+        })
+        // if repsonse is ok send data to displayCocktailData(drink)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                        parseCocktailData(data.drinks);
+                    })
+                    // if there is a response error
+            } else {
+                console.error("Response Failed");
+                displayModal("Bad Response");
+            }
+        })
+
+    // if there is a fetch error
+    .catch(err => {
+        console.error("Fetch Failed");
+        displayModal("Failed to get information. Make sure you are connected to the internet.");
+    });
+};
+
 const calcMaxMealSize = (availableHeight) => {
     let maxSize = 0;
     // find available width of screen
@@ -801,6 +843,90 @@ const displayModal = (msg) => {
             $('#popModal').remove();
         }
     });
+};
+
+//  ****** DISPLAY COCKTAIL DATA  ****** //
+var parseCocktailData = function(drinks) {
+    //console.log(drinks)
+    const myDrinks = drinks.map(el => { // create new array 'myDrinks' and remap old JSON data
+
+        let details = { // default object
+            "strIngredients": [], // array in array
+            "strMeasures": [] // array in array
+        }
+        Object.keys(el).forEach(key => {
+            if (key.startsWith('strIngredient')) { // search for keys that start with strIngredient and put them in new array 'strIngredients'
+                if (el[key]) // prevent ""
+                    details.strIngredients.push(el[key])
+            } else if (key.startsWith('strMeasure')) { // search for keys that start with strMeasures and put them in new array 'strMeasures'
+                if (el[key]) // prevent ""
+                    details.strMeasures.push(el[key])
+            } else {
+                details[key] = el[key] // add the rest of the keys/values to the new myDrinks array
+            }
+        })
+
+        return details
+    })
+
+    // console.log(myDrinks);
+    // console.log(myDrinks[0].strDrink);
+    // console.log(myDrinks[0]);
+
+    //calling the drink modal display and passing myDrinks through it
+    displayCocktails(myDrinks);
+
+};
+
+//displaying the drink modal
+const displayCocktails = (myDrinks) => {
+
+    // details of drink for modal
+
+    let modalDrinkDetails = $('<div>').attr('id', 'drinkDetails').addClass('modal-card-body');
+
+    let pic = $('<img>').attr('src', myDrinks[0].strDrinkThumb);
+    let videoDiv = $('<div>').append($('<a>').attr('href', myDrinks[0].strVideo).text('Video Link'));
+    // let video = $('<a>').attr('id', 'player');
+    let instructions = $('<p>').append("<hr>" + myDrinks[0].strInstructions);
+    let ingredientList = $('<div>').addClass('ingredient-list').append("Ingredients:");
+
+    let myIngredients = myDrinks[0].strIngredients;
+    let myMeasurments = myDrinks[0].strMeasures;
+
+    for (let i = 0; i < myIngredients.length; i++) {
+        let ingredientListBox = $("<div class='ingredient-checklist-holder'>");
+        let chkBoxItem = $('<input>').addClass('checkbox')
+            .attr('type', 'checkbox')
+            .attr('name', 'food')
+            .attr('id', myIngredients[i])
+            .attr('value', myIngredients[i]);
+
+        let item = $('<label>')
+            .attr('for', myIngredients[i])
+            .addClass('ingredient-item')
+            .addClass('checkbox')
+            .text(myIngredients[i])
+            .append(
+                $('<span>')
+                .addClass('ingredient-measure')
+                .text(myMeasurments[i])
+            );
+        ingredientListBox.append(chkBoxItem, item);
+        ingredientList.append(ingredientListBox);
+    }
+
+    modalDrinkDetails.append(pic, ingredientList, instructions);
+
+    displayModal({ head: myDrinks[0].strDrink, body: modalDrinkDetails, button: 'alert' });
+
+    $('[id^="drink-modal-close"]').click(function() {
+        //alert("Hello");
+
+        $('#drinkModal').remove();
+        $()
+    })
+
 };
 
 //checking if the meal was added to the favourite list
